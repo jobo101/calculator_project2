2f565524c84ed63c66543c22d89f1cb94088128d
Your Name <you@example.com>
2021-11-08 19:54:33 +0000

    AS: runner file & hello world

---
+++ src/com/qa/day1/Runner.java
@@ -0,0 +1,17 @@
+package com.qa.day1;
+
+public class Runner {
+	
+ public static void main(String[] args) {
+	
+	 System.out.println("Hello world!");
+	 
+	 System.out.println("");
+		   
+}
+	
+}	
+	 
+	 
+	
+
---
+++ src/com/qa/exercises/Runner.java
@@ -0,0 +1,19 @@
+package com.qa.exercises;
+
+public class Runner {
+	public static void main(String[] args) {
+		
+		System.out.println(Calculator.add(15, 28));
+		
+		System.out.println(Calculator.multiply(6, 90));
+		
+		System.out.println(Calculator.subtract(70 , 9));
+		
+		System.out.println(Calculator.divideInt(10, 2));
+		
+		System.out.println(Calculator.divide(9, 4));
+		
+		
+	}
+
+}
